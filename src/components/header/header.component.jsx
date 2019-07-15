7a25043e65e53cbf1fc32116e4adf3c50615bc8c
Philipp Mossier <p.mossier.development@gmail.com>
2019-07-15 11:38:26 +0200

    added cart-icon component into header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./header.styles.scss";
 
@@ -23,6 +24,7 @@ const Header = ({ currentUser }) => (
                     :
                     <Link className="option" to="/signin">SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 );
